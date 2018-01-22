0d706246c07eb51cfb777900dca80a371ee5d6d6
AlexSvast <svast@bk.ru>
2018-01-22 21:07:22 +0300

    Initial commit

---
+++ index.php
@@ -0,0 +1,12 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Svast
+ * Date: 22.01.2018
+ * Time: 20:50
+ */
+
+$output = json_decode(file_get_contents('php://input'),true);
+$id = $output['message']['chat']['id'];
+file_get_contents("https://api.telegram.org/bot525653361:AAHgI_H7PqBsAnVaeSUzQRllkQE8wUEjShY/sendMessage?chat_id=".$id."&text=hello");
+file_put_contents("logs.txt",$id);
