4ebf2ae58ab78a378fd46bb94f02a8cfe7e50ee6
AlexSvast <svast@bk.ru>
2018-01-22 21:15:48 +0300

    a

--- index.php
+++ index.php
@@ -8,5 +8,6 @@
 
 $output = json_decode(file_get_contents('php://input'),true);
 $id = $output['message']['chat']['id'];
-file_get_contents("https://api.telegram.org/bot525653361:AAHgI_H7PqBsAnVaeSUzQRllkQE8wUEjShY/sendMessage?chat_id=".$id."&text=hello");
+$text = $update['message']['text'];
+file_get_contents("https://api.telegram.org/bot525653361:AAHgI_H7PqBsAnVaeSUzQRllkQE8wUEjShY/sendMessage?chat_id=".$id."&text=".$text.);
 file_put_contents("logs.txt",$id);
