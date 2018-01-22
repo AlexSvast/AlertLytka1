0990e5ba5d161e21e2abef34293e87854807b780
AlexSvast <svast@bk.ru>
2018-01-22 21:25:02 +0300

    a

--- index.php
+++ index.php
@@ -11,3 +11,5 @@ $id = $output['message']['chat']['id'];
 $text = $output['message']['text'];
 file_get_contents("https://api.telegram.org/bot525653361:AAHgI_H7PqBsAnVaeSUzQRllkQE8wUEjShY/sendMessage?chat_id=".$id."&text=".$text."");
 file_put_contents("logs.txt",$id);
+
+if ($text='/r4') echo file_get_contents("https://api.telegram.org/bot525653361:AAHgI_H7PqBsAnVaeSUzQRllkQE8wUEjShY/sendMessage?chat_id=".$id."&text=Рейды уровня 4");
